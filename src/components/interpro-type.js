3114e8a5f362b7fc6505253c1d3cff2e8dca2285
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2017-07-18 10:25:18 +0100

    size and alignment updates for interpro-entry

--- src/components/interpro-type.js
+++ src/components/interpro-type.js
@@ -114,61 +114,77 @@ class InterproType extends HTMLElement {
       this.attachShadow({ mode: 'open' });
     }
     (this.shadyRoot || this.shadowRoot).innerHTML = `
-      <svg viewBox="0 0 72 72" width="${this._width}">
-        <defs>
-          <clipPath id="cut-off-center"><rect x="33%" y="33%" width="70" height="70"></rect></clipPath>
-          <clipPath id="cut-off-bottom"><polygon points="0,68 68,0 68,68"></polygon></clipPath>
-        </defs>
-        <rect
-          x="12" y="12"
-          width="60" height="60"
-          style="fill: black; opacity: 0.15"
-        />
-        <rect
-          x="4" y="4"
-          width="60" height="60"
-          stroke-width="8"
-          stroke="${this._type.colors[0]}"
-          fill="${this._type.colors[1]}"
-        />
-        <polygon points="0,68 68,0 68,68" fill="${this._type.colors[2]}" />
-        <text
-          x="50%" y="50%"
-          text-anchor="middle"
-          dx="-2px" dy="20px"
-          style="
-            fill: white;
-            font-size: 60px;
-            font-weight: 700;
-            font-family: 'Montserrat', 'arial', 'serif';
-          "
-        >
-          ${this._type.small}
-        </text>
-        <text
-          x="50%" y="50%"
-          text-anchor="middle"
-          dx="-2px" dy="20px"
-          clip-path="url(#cut-off-bottom)"
-          style="
-            fill: #e6e6e6;
-            font-size: 60px;
-            font-weight: 700;
-            font-family: 'Montserrat', 'arial', 'serif';
-          "
-        >
-          ${this._type.small}
-        </text>
-      </svg>
-      ${this.expanded
-        ? `
-          <span class="full"
-            style="background: none; color: ${this._type.color};"
+      <style>
+        div.container {
+          display: -webkit-flex; /* Safari */
+          -webkit-align-items: center; /* Safari 7.0+ */
+          display: inline-flex;
+          align-items: center;
+          -webkit-justify-content:center;
+          justify-content:center; 
+        }
+      div.container>svg {
+        vertical-align: middle;
+      }
+
+      </style>
+      <div class="container">
+        <svg viewBox="0 0 72 72" width="${this._width}">
+          <defs>
+            <clipPath id="cut-off-center"><rect x="33%" y="33%" width="70" height="70"></rect></clipPath>
+            <clipPath id="cut-off-bottom"><polygon points="0,68 68,0 68,68"></polygon></clipPath>
+          </defs>
+          <rect
+            x="12" y="12"
+            width="60" height="60"
+            style="fill: black; opacity: 0.15"
+          />
+          <rect
+            x="4" y="4"
+            width="60" height="60"
+            stroke-width="8"
+            stroke="${this._type.colors[0]}"
+            fill="${this._type.colors[1]}"
+          />
+          <polygon points="0,68 68,0 68,68" fill="${this._type.colors[2]}" />
+          <text
+            x="50%" y="50%"
+            text-anchor="middle"
+            dx="-2px" dy="20px"
+            style="
+              fill: white;
+              font-size: 60px;
+              font-weight: 700;
+              font-family: 'Montserrat', 'arial', 'serif';
+            "
           >
-            ${this._type.full}
-          </span>
-        `
-        : ''}
+            ${this._type.small}
+          </text>
+          <text
+            x="50%" y="50%"
+            text-anchor="middle"
+            dx="-2px" dy="20px"
+            clip-path="url(#cut-off-bottom)"
+            style="
+              fill: #e6e6e6;
+              font-size: 60px;
+              font-weight: 700;
+              font-family: 'Montserrat', 'arial', 'serif';
+            "
+          >
+            ${this._type.small}
+          </text>
+        </svg>
+        ${this.expanded
+          ? `
+            <span class="full"
+              style="background: none; color: ${this._type.color};"
+            >
+              ${this._type.full}
+            </span>
+          `
+          : ''}
+      </div>
     `.trim();
   }
 
@@ -222,7 +238,7 @@ class InterproType extends HTMLElement {
     // set defaults
     this._type = supportedTypes.get('unknown');
     this._expanded = false;
-    this._width = this.getAttribute('width') || '2em';
+    this._width = this.getAttribute('width') || '1em';
     this._handleLoadEvent = this._handleLoadEvent.bind(this);
     this._render = this._render.bind(this);
   }
